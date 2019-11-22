ff8e6c13abc6d523cb298146eb85a066b4dcf3c5
Tyrion Austin <tyrion.austin@ufl.edu>
2019-11-22 02:27:06 -0600

    Add javascript file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var hideForm = function( {
+  // to hide
+  var form = document.getElementById('hero-cta__form');
+  form.style.display = 'none';
+  
+})
+document.getElementById('hero-cta__form').onsubmit = funtion() {
+
+}
