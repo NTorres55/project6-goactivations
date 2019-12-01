c94d679e5c44e7848f8042570088859c40615348
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-12-01 18:54:21 -0500

    add shape to homepage

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById('submit');
-
-var myFunction = function() {
-  document.getElementById('form').style.display="none";
-  document.getElementById('thanks').style.display="block";
-}
-
-button.onclick = myFunction;
